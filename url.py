bddb7aa4197daad7382ec992b7b0ebe120bcd0ab
bkruthik <balusukruthik@gmail.com>
2026-03-18 22:28:46 +0530

    Create url.py

---
+++ url.py
@@ -0,0 +1,20 @@
+# Phishing URL Detection System A basic code to check the website is safe or not
+url = input("Enter a URL: ")
+print("\nChecking the URL for phishing indicators...\n")
+
+if url.startswith("http://"):
+    print("⚠ Insecure protocol detected (HTTP).")
+else:
+    print("✓ Secure protocol (HTTPS).")
+
+if "@" in url or "-" in url:
+    print("⚠ Unusual symbols found in the URL.")
+else:
+    print("✓ No unusual symbols detected.")
+
+if url.count(".") > 3:
+    print("⚠ Suspicious domain structure detected.")
+else:
+    print("✓ Domain structure looks normal.")
+
+print("\nAnalysis Complete.")
